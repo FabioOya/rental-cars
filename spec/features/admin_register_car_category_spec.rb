5abe4c8ba16039b8c299a378e472327e354f71c9
Fabio Oya <fabio_iwao@hotmail.com>
2020-04-09 13:53:52 -0300

    Admin visualiza categorias

--- spec/features/admin_register_car_category_spec.rb
+++ spec/features/admin_register_car_category_spec.rb
@@ -14,15 +14,15 @@ feature 'Admin register car category' do
         click_on 'Registrar categoria'
 
         fill_in 'Nome', with: 'Compactos'
-        fill_in 'Taxa Diaria', with: 250.50
-        fill_in 'Seguro', with: 100
+        fill_in 'Taxa Diaria', with: 200
+        fill_in 'Seguro', with: 100.50
         fill_in 'Seguro de terceiros', with: 120
         click_on 'Enviar'
 
         expect(current_path).to eq car_category_path(CarCategory.last.id)
         expect(page).to have_content('Compactos')
-        expect(page).to have_content(250,50)
-        expect(page).to have_content(100)
+        expect(page).to have_content(200)
+        expect(page).to have_content(100.50)
         expect(page).to have_content(120)
         expect(page).to have_link('Voltar')
     end
---
+++ spec/features/admin_view_car_categories_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+feature 'Admin view car categories' do
+    scenario 'succesfully' do
+        CarCategory.create!(name: 'Compactos', daily_rate: 200, car_insurance: 100.50, third_party_insurance: 120)
+        CarCategory.create!(name: 'Familia', daily_rate: 300, car_insurance: 150, third_party_insurance: 120)
+
+        visit root_path
+        click_on 'Categorias de carros'
+
+        expect(page).to have_content('Compactos')
+        expect(page).to have_content('Familia')
+    end
+
+    scenario 'and view details' do
+        CarCategory.create!(name: 'Compactos', daily_rate: 200, car_insurance: 100.50, third_party_insurance: 120)
+        CarCategory.create!(name: 'Familia', daily_rate: 300, car_insurance: 150, third_party_insurance: 120)
+
+        visit root_path
+        click_on 'Categorias de carros'
+        click_on 'Compactos'
+
+        expect(page).to have_content('Compactos')
+        expect(page).to have_content(200)
+        expect(page).to have_content(100.50)
+        expect(page).to have_content(120)
+        expect(page).not_to have_content('Familia')
+        expect(page).not_to have_content(300)
+        expect(page).not_to have_content(150)
+    end
+
+    scenario 'and no car categories are created' do
+        visit root_path
+        click_on 'Categorias de carros'
+
+        expect(page).to have_content('Nenhuma categoria cadastrada')
+    end
+    
+    scenario 'and return to car categories page' do
+        CarCategory.create!(name: 'Compactos', daily_rate: 200, car_insurance: 100.50, third_party_insurance: 120)
+        CarCategory.create!(name: 'Familia', daily_rate: 300, car_insurance: 150, third_party_insurance: 120)
+
+        visit root_path
+        click_on 'Categorias de carros'
+        click_on 'Compactos'
+        click_on 'Voltar'
+
+        expect(current_path).to eq car_categories_path
+    end
+    
+end
